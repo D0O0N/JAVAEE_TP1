817724aeca82e80541cb526730726d010a36f025
pedago <pedago@pcalb-mm0607.univ-jfc.fr>
2019-09-17 15:57:24 +0200

    Yolo ça arrive

---
+++ src/main/java/NouveauPackage/NouveauClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NouveauPackage;
+
+/**
+ *
+ * @author pedago
+ */
+// Cette classe est la meilleure parceque c'est la classe d'info MDR
+public class NouveauClass {
+    
+}
